0b68bf7790a226dc3402107dbc7cd818ff65496d
qnicole-zakka <54155870+qnicole-zakka@users.noreply.github.com>
2020-04-17 16:33:08 -0700

    Create learn-tf-keras.py

---
+++ learn-tf-keras.py
@@ -0,0 +1,2 @@
+import tensorflow as tf
+# https://colab.research.google.com/github/google/eng-edu/blob/master/ml/cc/exercises/linear_regression_with_synthetic_data.ipynb?utm_source=mlcc&utm_campaign=colab-external&utm_medium=referral&utm_content=linear_regression_synthetic_tf2-colab&hl=en#scrollTo=_GMGgR6O54IN
