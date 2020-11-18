303043a65b1a74d2c786196ca74e0053f84bcee4
qnicole-zakka <54155870+qnicole-zakka@users.noreply.github.com>
2020-11-18 15:27:11 -0800

    Create dunder.py

---
+++ dunder.py
@@ -0,0 +1,31 @@
+# Learn the functions of different underscores
+# 5 types: bare, combinations of leading/trailing and single/double
+############################### 
+# 1. bare underscore 
+############################### 
+
+############################### 
+# 2. single leading
+############################### 
+## internal use
+## only can be functional recognize when in the following case 1
+# case 1:
+# This is the my_mod.py
+def int_func():
+    print('internal')
+def ext_func():
+    print('external')
+# 
+ 
+ 
+############################### 
+# 3. double leading
+############################### 
+
+############################### 
+# 4. single training
+############################### 
+
+############################### 
+# 5. double trailing
+############################### 
