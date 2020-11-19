130986a5a8891c4d8474260b752e68952c5054b8
qnicole-zakka <54155870+qnicole-zakka@users.noreply.github.com>
2020-11-18 16:00:32 -0800

    Update dunder.py

--- dunder.py
+++ dunder.py
@@ -1,4 +1,5 @@
 # Learn the functions of different underscores
+# https://dbader.org/blog/meaning-of-underscores-in-python
 # 5 types: bare, combinations of leading/trailing and single/double
 ############################### 
 # 1. bare underscore 
@@ -11,11 +12,20 @@
 ## only can be functional recognize when in the following case 1
 # case 1:
 # This is the my_mod.py
-def int_func():
+def _int_func():
     print('internal')
 def ext_func():
     print('external')
-# 
+# import method 1: _ not enforced by the Python interpreter
+import my_mod
+my_mod._int_func() 
+my_mod.ext_func()
+
+# import method 2: enforced
+# but * does not match coding convention, should use method 1 or explicit class/function names
+from my_mod import *
+_int_func() # error
+ext_func() 
  
  
 ############################### 
@@ -23,9 +33,11 @@ def ext_func():
 ############################### 
 
 ############################### 
-# 4. single training
+# 4. single trailing
 ############################### 
-
+## to avoid clashing with reserved keywords
+def init_class(t, class_):  
+    print('making class')
 ############################### 
 # 5. double trailing
 ############################### 
