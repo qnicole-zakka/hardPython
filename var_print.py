b5d992172d3507217120a17edcfb292cb4b80b67
qnicole-zakka <qnicole87@yahoo.com>
2022-06-06 16:05:13 -0700

    revive in 2022

---
+++ var_print.py
@@ -0,0 +1,19 @@
+my_name = "Zed"
+my_height = 60
+
+print(f"My name is: {my_name}")
+print(f'Height is {my_height}')
+
+type_of_people = 10
+x = f'There are {type_of_people} types of people'
+print(x)
+
+s1 = "The left of a sentence ... "
+s2 = "the right of a sentence"
+print(s1 + s2)
+
+print('.' * 10)
+print('abc' * 3)
+
+print('Burger' + '--')
+print('Burger', '--')
